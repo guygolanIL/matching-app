ba9c7fed53fea917976abbe426173cb8565e30c2
Guy Golan <guygolan91@gmail.com>
2022-09-16 12:18:27 +0300

    added test get

--- index.ts
+++ index.ts
@@ -5,6 +5,10 @@ const app: Application = express();
 
 app.use(json());
 
+app.get('/', (req, res) => {
+    res.send('hi');
+});
+
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 });
